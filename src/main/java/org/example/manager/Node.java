04a6cdf796abc3802948fea907d548bcf2d2a153
User Sakhan Aset <assetsakhan@yandex.kz>
2025-04-29 01:44:10 +0500

    Исправление Checkstyle: пустая строка в Node.java

--- src/main/java/org/example/manager/Node.java
+++ src/main/java/org/example/manager/Node.java
@@ -1,14 +1,13 @@
 package org.example.manager;
-import  org.example.model.Task;
-public class Node {
-    Task data;
-    Node prev;
-    Node next;
 
-        Node(Task data) {
-            this.data = data;
-            this.prev = null;
-            this.next = null;
-        }
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+    public Node<T> prev;
 
+    public Node(Node<T> prev, T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
 }
